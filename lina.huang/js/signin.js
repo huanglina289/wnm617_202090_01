0b1d46280a516a33dcb1b1078f089d79819fbb70
huanglina289 <61169528+huanglina289@users.noreply.github.com>
2020-10-15 13:07:53 -0700

    module 6

---
+++ lina.huang/js/signin.js
@@ -0,0 +1,52 @@
+
+const makeWarning = (target,message) => {
+   $(target).addClass("active")
+      .find('.message').html(message);
+   setTimeout(()=>{
+      $(target).removeClass("active");
+   },2000);
+}
+
+
+const checkSigninForm = () => {
+   let user = $("#signin-username").val();
+   let pass = $("#signin-password").val();
+
+   if(user=='' || pass=='') {
+      makeWarning("#signin-warning","Please fill in Username and Password");
+      return;
+   }
+
+   console.log(user,pass)
+
+   if(user == 'user' && pass == 'pass') {
+      // logged in
+      console.log("sucess");
+      sessionStorage.userId = 3;
+
+      $("#signin-form")[0].reset();
+   } else {
+      // not logged in
+      console.log("failure");
+      sessionStorage.removeItem('userId');
+
+   // ASSIGNMENT -> how to tell user if they fail signin?
+      makeWarning("#signin-warning","Incorrect Username or Password")
+   }
+
+   checkUserId();
+}
+
+const checkUserId = () => {
+   let p = ['#signin-page','#signup-page',''];
+
+   if(sessionStorage.userId === undefined) {
+      // not logged in
+      if(!p.some(o=>window.location.hash===o))
+         $.mobile.navigate("#signin-page");
+   } else {
+      // logged in
+      if(p.some(o=>window.location.hash===o))
+         $.mobile.navigate("#recent-page");
+   }
+}
